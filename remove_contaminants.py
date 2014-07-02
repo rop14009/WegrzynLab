d532da96d7cba482c3b8772282a264bf994d685b
Ethan Baker <user@d89h10.public.uconn.edu>
2014-07-02 12:04:32 -0400

    working version
    now removes fasta records that correspond to a list of contaminated
    seqs. Relies on the contaminated-*.txt file from job.py. Properly
    formatted list is generated with the remove_contaminants_pipe.sh which
    will also execute the python code. Outputs a file called
    “ucontaminantedseqs.fasta”

--- remove_contaminants.py
+++ remove_contaminants.py
@@ -1,18 +1,16 @@
-#This script is unfinished.
+#Script relies on a list of uncontaminated names. Must be 
 #Start with list of uncontaminated header lines, could pipe through w/ bash file
 from Bio import SeqIO
 
 #Import list of header lines.
 headerlines=[]
-with open('clean_hits.names', 'r') as namefile:
+with open('contaminants.names', 'r') as namefile:
 	headerlines = namefile.read().splitlines()
-	#Checked to see if above code works, it does. 
-	#print headerlines
 
 #Go through each record in the fasta file and if a record's header line matches, add it to a list.
 matches=[]
 for record in SeqIO.parse("centroids.fasta.transdecoder.cds", "fasta"):
-	if record.id in headerlines:
+	if record.id not in headerlines: #Records not in headers of contaminants must be clean hits/
 		matches.append(record)
 #Print to an output fasta file
 output=open("uncontaminatedseqs.fasta", "w")
--- remove_contaminants_pipe.sh
+++ remove_contaminants_pipe.sh
@@ -1,8 +1,15 @@
-#This code is unfinished. 
+#!/bin/bash -l
+#SBATCH -J rmcontam
+#SBATCH -o rmcontam-%j.output
+#SBATCH -e rmcontam-%j.err
+#SBATCH -n 4
+#SBATCH -N 1
+#SBATCH -p bigmem
+
 module load python
 module load Biopython
 
-cut -f1 AnnotationReport-* | cut -f1 -d " " > clean_hits.names
+cut -f1 contaminated-*.txt | cut -f1 -d " " > contaminants.names
 
 python remove_contaminants.py
 
