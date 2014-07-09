77430acea1752fb09a45ab0bff812a14d5a9d1ed
Ethan Baker <user@d89h218.public.uconn.edu>
2014-07-09 15:52:46 -0400

    Allows user to input FASTA file path.

--- remove_contaminants.py
+++ remove_contaminants.py
@@ -1,5 +1,6 @@
-#Script relies on a list of uncontaminated names. Must be 
-#Start with list of uncontaminated header lines, could pipe through w/ bash file
+#Purpose: Removes seqs annotated as contaminants from full length genes file. 
+#Script relies on a list of uncontaminated names. See remove_contaminants_pipe.sh
+#Computational Genomics Lab, University of Connecticut, Storrs 
 from Bio import SeqIO
 
 #Import list of header lines.
@@ -9,7 +10,7 @@ with open('contaminants.names', 'r') as namefile:
 
 #Go through each record in the fasta file and if a record's header line matches, add it to a list.
 matches=[]
-for record in SeqIO.parse("centroids.fasta.transdecoder.cds", "fasta"):
+for record in SeqIO.parse(raw_input("Enter FASTA file path (relative OK): "), "fasta"):
 	if record.id not in headerlines: #Records not in headers of contaminants must be clean hits/
 		matches.append(record)
 #Print to an output fasta file
