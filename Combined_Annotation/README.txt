fd2bb84767f9ca97c7ba852607d4492b3ea66a39
SamGinzburg <samuel.ginzburg@uconn.edu>
2014-10-05 16:04:59 -0400

    Many changes/fixes

--- Combined_Annotation/README.txt
+++ Combined_Annotation/README.txt
@@ -1,16 +1,37 @@
-This script takes the blast2go, interpro, and annotation input and merges them together into one file, please place the three files into the "input" folder before starting
+This script takes the blast2go, interpro, and annotation input and merges them together into one file
+
+@PARAMETERS
+
+--blast2go, --interpro, --o, --h
+
+The blast2go and interpro command line options indicate whether you will be using one, the other, or both to generate the combined annotation
+
+If you use both of these command line arguments you should type the initial annotation file name, then the blast2go file, then the interpro file
+
+
+IF the --o flag is not used the default output file name is combine_annotations.tsv
+
 
 ***NOTE***
 
-The files in the input directory must be named in the following manner:
+In all situations if the --o command line argument is used to generate a custom file name for the output the last argument for the script MUST BE THE NEW FILE NAME, otherwise the script will terminate
 
-annotation.tsv ---> the annotation file
-blast2go.txt   ---> the blast2go file
-interpro.raw   ---> the interpro file
+In addition to this if the --h help flag is used, the the script will print out a series of examples usages but will then TERMINATE, regardless of the rest of the flags/arguments
 
 ***NOTE***
+
+Example usages of the script that will result in correct outputs:
+
+
+py combine_annotations.py --blast2go --interpro annotation.tsv blast2go.txt interpro.raw output_file_name.tsv
+
+
+py combine_annotations.py --interpro annotation.tsv interpro.raw
+
+
+py combine_annotations.py --blast2go annotation.tsv blast2go.txt output_file_name.tsv
 
 
 When the script finishes running, the output file will be availible in the output directory.
 
-(output file name is combined_annotation.tsv)
+
--- Combined_Annotation/combine_annotations.py
+++ Combined_Annotation/combine_annotations.py
@@ -3,8 +3,8 @@ Author: Sam Ginzburg
 
 Description: An implementation of a program that combines blast2go, interpro, and previous annotation files
 
-@param The input files should all be placed in the 'input' directory adjacent to the location of this script
-
+@param 	at least one of the following flags are required to run the program:  --blast2go --interpro --o , (--o : the output file name is optional, but if included MUST be present)
+		
 '''
 
 import time
@@ -35,66 +35,207 @@ def parse_blast2go_column(row):
     return return_string[:-2] # substring taken to just remove the last ; deliminator for aesthetic reasons
 
 def make_blast2go_walnut_combined_dict(file_name):
-    blast2go_data = dict()
-    with open(file_name,'r') as tsv_old:
-        row = next(tsv_old) #skip the first header line
-        for row in csv.reader(tsv_old, delimiter='\t'):
-            id = combined_anno_id_parser(row[0]) #get the key
-            blast2go_data[id] = parse_blast2go_column(row)
-    print("blast2go hashtable complete")
-    return blast2go_data
+	blast2go_data = dict()
+	with open(file_name,'r') as tsv_old:
+		row = next(tsv_old) #skip the first header line
+		for row in csv.reader(tsv_old, delimiter='\t'):
+			id = combined_anno_id_parser(row[0]) #get the key
+			blast2go_data[id] = parse_blast2go_column(row)
+	return blast2go_data
 
 def make_walnut_interpro_dict(file_name):
-    walnut_interpro_data = dict()
-    value_list = list()
-    with open(file_name,'r') as tsv_old:
-        for row in csv.reader(tsv_old, delimiter='\t'):
-            id = row[0][8:] #get the key
-            value_list.append(row[5])
-            value_list.append(row[11])
-            value_list.append(row[12])
-            walnut_interpro_data[id] = value_list
-            value_list = list()
-    print("walnut_interpro hastable complete")
-    
-    return walnut_interpro_data
+	walnut_interpro_data = dict()
+	value_list = list()
+	print (file_name)
+	with open(file_name,'r') as tsv_old:
+		for row in csv.reader(tsv_old, delimiter='\t'):
+			id = row[0][8:] #get the key
+			value_list.append(row[5])
+			value_list.append(row[11])
+			value_list.append(row[12])
+			walnut_interpro_data[id] = value_list
+			value_list = list()
+	return walnut_interpro_data
+
+def print_usage():
+	print("To run this script use at least one of the following flags:")
+	print("--blast2go --interpro")
+	print("EX: py combine_annotations.py --blast2go --interpro annotation.tsv blast2go.txt interpro.raw output_file_name.tsv")
+	print("EX: py combine_annotations.py --interpro annotation.tsv interpro.raw")
+	print("EX: py combine_annotations.py --blast2go annotation.tsv blast2go.txt output_file_name.tsv")
+	sys.exit(-1)
+
+def parse_flags(param_list):
+	blast2go = 0
+	interpro = 0
+	custom_output_name = 0
+	for param in param_list:
+		if param == "--h":
+			print_usage()
+		elif param == "--blast2go":
+			blast2go += 1
+		elif param == "--interpro":
+			interpro += 1
+		elif param == "--o":
+			custom_output_name += 1
+	
+	if interpro == 1 and blast2go == 1:
+		if custom_output_name == 1:
+			return "all_params_custom_output"
+		return "all_params"
+	elif interpro == 1 and blast2go == 0:
+		if custom_output_name == 1:
+			return "interpro_custom_output"
+		return "interpro"
+	elif interpro == 0 and blast2go == 1:
+		if custom_output_name == 1:
+			return "blast2go_custom_output"
+		return "blast2go"
+	else:
+		print_usage()
+		return "incorrect_parameters"
 
 if __name__ == '__main__':
-    start_time = time.clock()
-    
+	start_time = time.clock()
+	arguments_list = sys.argv
+	params = parse_flags(arguments_list)
+	print (arguments_list)
+	
+	
+	if params == "all_params" or params == "all_params_custom_output":
+	
+	
+		if params == "all_params_custom_output":
+			output = arguments_list[len(arguments_list) - 1]
+			file_path_combined_anno = arguments_list[4]
+			file_path_blast2go_walnut = arguments_list[5]
+			file_path_walnut_interpro = arguments_list[6]
+			
+			if output == file_path_walnut_interpro or output == file_path_combined_anno or output == file_path_blast2go_walnut:
+				print ("WARNING user entered option for custom file name and entered name of existing file (would cause overwrite) -- aborting")
+				print_usage()
+				sys.exit(-1)
+		else:
+			output = "combine_annotations.tsv"
+			file_path_combined_anno = arguments_list[3]
+			file_path_blast2go_walnut = arguments_list[4]
+			file_path_walnut_interpro = arguments_list[5]
+		
 
-    file_path_combined_anno = os.path.dirname(os.path.realpath(__file__)) + '//input//annotation.tsv'
-    file_path_blast2go_walnut = os.path.dirname(os.path.realpath(__file__)) + '//input//blast2go.txt'
-    file_path_walnut_interpro = os.path.dirname(os.path.realpath(__file__)) + '//input//interpro.raw'
+		
+		#first make dicts for walnut interpro and blast2go for easier searching based off of IDs
+		walnut_interpro_hashtable = make_walnut_interpro_dict(file_path_walnut_interpro)
+		blast2go_hastable = make_blast2go_walnut_combined_dict(file_path_blast2go_walnut)
+		
+		with open(file_path_combined_anno,'r') as tsv_old, \
+		open(os.path.dirname(os.path.realpath(__file__)) + "//" + output, 'w') as tsv_new:
+			tsv_new = csv.writer(tsv_new, delimiter='\t')
+			tsv_old = csv.reader(tsv_old, delimiter='\t')		
+			row = next(tsv_old) # the purpose of this line is to skip the header in the csv file, so there is no need to iterate another 10K plus times through blast2go_table_walnut/walnut_interpro
+			combined_row = row + ["walnut 5", "walnut 11", "walnut 12", "blast2go_process_column"]
+			tsv_new.writerow(combined_row) # copy the header
+			for row in tsv_old:
+				id = combined_anno_id_parser(row[0])
+				
+				walnut_results_interpro = walnut_interpro_hashtable.get(id)
+				
+				if not walnut_results_interpro: #this is to create the blank spaces if there are no interpro results for corresponding IDs
+					walnut_results_interpro = ["N/A","N/A","N/A"]
+					
+				walnut_results_blast2go = blast2go_hastable.get(id)
+				
+				if not walnut_results_blast2go: # if there is no match from blast2go use as filler
+					walnut_results_blast2go = "N/A"
+					
+				combined_row = row + walnut_results_interpro + [walnut_results_blast2go]
+				tsv_new.writerow(combined_row)	
+				
+	elif params == "interpro" or params == "interpro_custom_output":
+		
+		print (arguments_list[len(arguments_list) - 1])
+		
+		if params == "interpro_custom_output":
+			output = arguments_list[len(arguments_list) - 1]
+			file_path_combined_anno = arguments_list[3]
+			file_path_walnut_interpro = arguments_list[4]
+			if output == file_path_walnut_interpro or output == file_path_combined_anno:
+				print ("WARNING user entered option for custom file name and entered name of existing file (would cause overwrite) -- aborting")
+				print_usage()
+				sys.exit(-1)
+			
+		else:
+			output = "combine_annotations.tsv"
+			file_path_combined_anno = arguments_list[2]
+			file_path_walnut_interpro = arguments_list[3]
+		
+		print (output)
+		file_path_blast2go_walnut = "None"
+		
+		#first make dicts for walnut interpro and blast2go for easier searching based off of IDs
+		walnut_interpro_hashtable = make_walnut_interpro_dict(file_path_walnut_interpro)
+		
+		with open(file_path_combined_anno,'r') as tsv_old, \
+		open(os.path.dirname(os.path.realpath(__file__)) + "//" + output, 'w') as tsv_new:
+			print(os.path.realpath(__file__) + "//" + output)
+			tsv_new = csv.writer(tsv_new, delimiter='\t')
+			tsv_old = csv.reader(tsv_old, delimiter='\t')		
+			row = next(tsv_old) # the purpose of this line is to skip the header in the csv file, so there is no need to iterate another 10K plus times through blast2go_table_walnut/walnut_interpro
+			combined_row = row + ["walnut 5", "walnut 11", "walnut 12", "blast2go_process_column"]
+			tsv_new.writerow(combined_row) # copy the header
+			for row in tsv_old:
+				id = combined_anno_id_parser(row[0])
+				
+				walnut_results_interpro = walnut_interpro_hashtable.get(id)
+				
+				if not walnut_results_interpro: #this is to create the blank spaces if there are no interpro results for corresponding IDs
+					walnut_results_interpro = ["N/A","N/A","N/A"]
+					
+				combined_row = row + walnut_results_interpro + ["N/A"]
+				tsv_new.writerow(combined_row)		
+		
+	elif params == "blast2go" or params == "blast2go_custom_output":
+		
+		if params == "blast2go_custom_output":
+			output = arguments_list[len(arguments_list) - 1]
+			file_path_combined_anno = arguments_list[3]
+			file_path_blast2go_walnut = arguments_list[4]
+			if output == file_path_blast2go_walnut or output == file_path_combined_anno:
+				print ("WARNING user entered option for custom file name and entered name of existing file (would cause overwrite) -- aborting")
+				print_usage()
+				sys.exit(-1)
+		else:
+			output = "combine_annotations.tsv"
+			file_path_combined_anno = arguments_list[2]
+			file_path_blast2go_walnut = arguments_list[3]
+		
+	
+		file_path_walnut_interpro = ""
+		
+		#first make dicts for walnut interpro and blast2go for easier searching based off of IDs
+		blast2go_hastable = make_blast2go_walnut_combined_dict(file_path_blast2go_walnut)
+		
+		with open(file_path_combined_anno,'r') as tsv_old, \
+		open(os.path.dirname(os.path.realpath(__file__)) + "//" + output, 'w') as tsv_new:
+			tsv_new = csv.writer(tsv_new, delimiter='\t')
+			tsv_old = csv.reader(tsv_old, delimiter='\t')		
+			row = next(tsv_old) # the purpose of this line is to skip the header in the csv file, so there is no need to iterate another 10K plus times through blast2go_table_walnut/walnut_interpro
+			combined_row = row + ["walnut 5", "walnut 11", "walnut 12", "blast2go_process_column"]
+			tsv_new.writerow(combined_row) # copy the header
+			for row in tsv_old:
+				id = combined_anno_id_parser(row[0])
+				
+				walnut_results_interpro = ["N/A","N/A","N/A"]
+					
+				walnut_results_blast2go = blast2go_hastable.get(id)
+				
+				if not walnut_results_blast2go: # if there is no match from blast2go use as filler
+					walnut_results_blast2go = "N/A"
+					
+				combined_row = row + walnut_results_interpro + [walnut_results_blast2go]
+				tsv_new.writerow(combined_row)	
+	elif params == "incorrect_parameters":
+		sys.exit(-1)
+		
 
-    #first make dicts for walnut interpro and blast2go for easier searching based off of IDs
-    walnut_interpro_hashtable = make_walnut_interpro_dict(file_path_walnut_interpro)
-    blast2go_hastable = make_blast2go_walnut_combined_dict(file_path_blast2go_walnut)
-       
-        
-    #then go through old combined anno and line by line make new anno file from previous data
-       
-    with open(file_path_combined_anno,'r') as tsv_old, \
-    open(os.path.dirname(os.path.realpath(__file__)) + '//output//combined_annotation.tsv', 'w') as tsv_new:
-        tsv_new = csv.writer(tsv_new, delimiter='\t')
-        tsv_old = csv.reader(tsv_old, delimiter='\t')
-        row = next(tsv_old) # the purpose of this line is to skip the header in the csv file, so there is no need to iterate another 10K plus times through blast2go_table_walnut/walnut_interpro
-        combined_row = row + ["walnut 5", "walnut 11", "walnut 12", "blast2go_process_column"]
-        tsv_new.writerow(combined_row) # copy the header
-        for row in tsv_old:
-            id = combined_anno_id_parser(row[0])
-                
-            walnut_results_interpro = walnut_interpro_hashtable.get(id)
-                
-            if not walnut_results_interpro: #this is to create the blank spaces if there are no interpro results for corresponding IDs
-                walnut_results_interpro = ["N/A","N/A","N/A"]
-               
-            walnut_results_blast2go = blast2go_hastable.get(id)
-                
-            if not walnut_results_blast2go: # if there is no match from blast2go use as filler
-                walnut_results_blast2go = "N/A"
-                
-            combined_row = row + walnut_results_interpro + [walnut_results_blast2go]
-            tsv_new.writerow(combined_row)
-    print (str(time.clock() - start_time) + " seconds")
-    print("complete -- file now availible in output directory")
+	print (str(time.clock() - start_time) + " seconds")
+	print("complete -- annotation file now available")
