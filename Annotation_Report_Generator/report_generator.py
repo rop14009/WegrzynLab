275eb300c9a5502386d9b4377c2b34c4f4452392
Samuel Ginzburg <samg@farm.cse.ucdavis.edu>
2014-10-20 09:33:01 -0700

    Updates to the script

--- Annotation_Report_Generator/report_generator.py
+++ Annotation_Report_Generator/report_generator.py
@@ -61,9 +61,10 @@ def get_gi_string(line):
 	temp_string = ""
 	for char in line:
 		if char == " ":
+			#print (temp_string[1:])
 			return temp_string[1:] # first char cut off since we know it to be ">"
 		temp_string += char
-	return temp_string		
+	return temp_string[1:]		
 	
 # this method creates hashtable used for looking up fasta seq based off of gi
 def multi_fasta_parse(file_name):
@@ -78,15 +79,21 @@ def multi_fasta_parse(file_name):
 		for line in file:
 			if line[:1] == ">": # line contains description of sequence
 				if current_protein_seq != "":
+					#print (current_gi)
 					fasta_db[current_gi] = current_protein_seq
 					fasta_db_description[current_gi] = current_desc
 					fasta_db_species[current_gi] = current_species
 					current_protein_seq = ""
 					current_desc = ""
 					current_species = ""
-				current_gi = get_gi_string(line)
-				current_desc = line[line.find(" "):].strip()
-				current_species = line[line.find("[")+1:line.find("]")]
+				
+				current_gi =  get_gi_num_from_string(get_gi_string(line))
+				if db_type == "ncbi":
+					current_desc = line[line.find(" "):].strip()
+					current_species = line[line.find("[")+1:line.find("]")]
+				else: #this info is not included in usearch fasta seq
+					current_desc = ""
+					current_species = ""
 			else:
 				current_protein_seq += line
 	return [fasta_db, fasta_db_description, fasta_db_species]
@@ -109,10 +116,12 @@ Field 12: bit score
 '''	
 def ncbi_format_db_parse(file_name):
 	ncbi_db = dict()
+	global num_queries
 	with open(file_name, "r") as file:
 		file_tsv = csv.reader(file, delimiter='\t')
 		for line in file_tsv:
-			ncbi_db[line[1]] = line
+			ncbi_db[ get_gi_num_from_string(line[1])] = line
+			num_queries += 1
 	return ncbi_db
 	
 	
@@ -122,10 +131,13 @@ find later
 '''	
 def usearch_format_db_parse(file_name):
 	usearch_db = dict()
+	global num_queries
 	with open(file_name, "r") as file:
 		file_tsv = csv.reader(file, delimiter='\t')
 		for line in file_tsv:
-			usearch_db[line[1]] = line
+			#print(get_gi_num_from_string(line[1]))
+			num_queries += 1
+			usearch_db[get_gi_num_from_string(line[1])] = line
 	return usearch_db
 	
 
@@ -147,6 +159,7 @@ def is_uninformative(fasta_db_element):
 def write_log(element, log_name):
 	if number_db == 1:
 		if not os.path.exists(log_name+".txt"): # if file doesnt exist create it
+			print ("creating new logfile with name: " + log_name)
 			file = open(log_name+".txt", "w")
 			file.write(element+"\n")
 			file.close()
@@ -157,6 +170,7 @@ def write_log(element, log_name):
 	else:
 		counter -= 1
 		if not os.path.exists(log_name+".txt"): # if file doesnt exist create it
+			print ("creating log file with name: " + log_name)
 			file = open(log_name+".txt", "w")
 			if not counter > 0:
 				file.write(element)
@@ -192,6 +206,7 @@ def write_contaminants_log(element,log_name):
 	if number_db == 1:
 		if not os.path.exists(log_name+".txt"): # if file doesnt exist create it
 			file = open(log_name+".txt", "w")
+			print ("creating contaminants log with name: " + log_name)
 			file.write(element+"\n")
 			file.close()
 		else:
@@ -202,6 +217,7 @@ def write_contaminants_log(element,log_name):
 		counter -= 1
 		if not os.path.exists(log_name+".txt"): # if file doesnt exist create it
 			file = open(log_name+".txt", "w")
+			print ("creating contaminants log with name: " + log_name)
 			if not counter > 0:
 				file.write(element)
 			else:
@@ -217,7 +233,10 @@ def write_contaminants_log(element,log_name):
 	
 	
 def match_fasta(fasta, fasta_desc, fasta_species, db):
-	
+        global num_queries_informative_hit #1 or more informative hits
+        global num_queries_no_hit #no hits
+        global num_queries_uninformative
+
 	for element in fasta:
 		#check for contaminants
 		if fasta_species[element] in contaminants:
@@ -231,14 +250,18 @@ def match_fasta(fasta, fasta_desc, fasta_species, db):
 			else:
 				print("this should not be reached")
 			del db[element]
-					
+		print(element)
+		print(db.get(element))			
 		#make sure there is a match between fasta element and db
 		if not db.get(element) is None:
 			#returns true if "uninformative"
 			#then if uninformative changes desc to uninformative
 			if is_uninformative(fasta_desc[element]):
 				fasta_desc[element] = "uninformative" + fasta_desc[element][fasta_desc[element].find("[")-1:]
-		
+				num_queries_uninformative += 1
+			else:
+				num_queries_informative_hit += 1
+
 			get = db.get(element) + [fasta_desc[element][:fasta_desc[element].find("[")]] + [fasta_species[element]]
 			if type(get) is list:
 				tsv_new.writerow(get)
@@ -247,7 +270,8 @@ def match_fasta(fasta, fasta_desc, fasta_species, db):
 			#after match is found remove it from db
 			del db[element]
 		else:
-			print("no such fasta in db")
+			print ("no such element in db: " + element)
+			num_queries_no_hit += 1
 	
 
 			
@@ -256,6 +280,7 @@ if __name__ == '__main__':
 	start_time = time.clock()
 	arguments_list = sys.argv
 	settings = parse_config_file("configuration_file.txt") #sets up the settings
+	output_log = "log"
 	global filter_list
 	global contaminants
 	global number_db
@@ -269,18 +294,32 @@ if __name__ == '__main__':
 	global shortest_query_length
 	global median_query_length
 	
+	longest_query_length = -2147483648
+	shortest_query_length = 2147483647
+	median_query_length = 0
+
 	global total_query_sequences
 	global avg_length_query_sequences
 	
+	total_query_sequences = 0
+	avg_length_query_sequences = 0
+
 	global num_queries # the number of queries
 	global num_queries_informative_hit #1 or more informative hits
 	global num_queries_no_hit #no hits
 	global num_queries_uninformative
 	
-	
+	num_queries = 0
+	num_queries_informative_hit = 0
+	num_queries_no_hit = 0
+	num_queries_uninformative = 0
+	print (num_queries)
+	global db_type
+	db_type = settings[2]
 	filter_list = load_filter_list("filter_list.txt")
 	contaminants = build_contaminants_db()
 	contaminants_found = dict()
+	print("contaminants db built")
 	output = "default_output_annotation.tsv"
 	number_db = int(settings[1]) # the number of databases being parsed
 	counter = number_db
@@ -321,6 +360,7 @@ if __name__ == '__main__':
 				write_log(db.get(key)[0],"nohits")
 			
 			for key in contaminants_found:
+				print (contaminants_found.get(key))
 				write_contaminants_log(contaminants_found.get(key),"contaminants")
 			
 		elif number_db == 2: #database/fasta pair 2
@@ -330,7 +370,17 @@ if __name__ == '__main__':
 			print("3 database/fasta pair")
 				
 		
-
 	
 	print (str(time.clock() - start_time) + " seconds")
 	print("complete -- annotation file now available")
+
+
+	if not os.path.exists(output_log+".txt"):
+		print("creating log file: " + output_log)
+		log_file = open(output_log + ".txt", "w")
+		file.write("num_queries: " + num_queries + "\n")
+		file.write("num_queries_informative_hit: " + num_queries_informative_hit + "\n")
+		file.write("num_queries_no_hit: " + num_queries_no_hit + "\n")
+                file.write("num_queries_uninformative: " + num_queries_uninformative + "\n")
+		file.close()
+		print ("log files complete -- exiting")
